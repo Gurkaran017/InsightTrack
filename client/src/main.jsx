8c4762532ae3a126aa66423376d97995465c409d
Gurkaran017 <gurkaransingh017gh@gmail.com>
2025-04-18 23:44:22 +0530

    InsightTrack

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-import './Index.css';
+import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext'; // ✅ Import the provider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider> {/* ✅ Wrap your App here */}
+    <AuthProvider> 
+      
       <App />
     </AuthProvider>
   </React.StrictMode>
 );
+//index.css
