de517174964f76bd4492ba929cde7231a856da23
Gurkaran017 <gurkaransingh017gh@gmail.com>
2025-04-18 23:29:31 +0530

    InsightTrack

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const router = require("./routes/authRoute");
+const dotenv = require('dotenv');
+dotenv.config();
+const path = require('path');
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", router);
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=>console.log("MongoDB connected"))
+.catch((err)=>console.log(`MongoDB not connected ${err}`));
+
+const _dirname = path.resolve();
+
+app.use((err, req, res, next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message,
+    });
+});
+
+app.use(express.static(path.join(_dirname, "/client/dist")))
+app.get("*" , (_,res) => {
+  res.sendFile(path.resolve(_dirname, "client" , "dist" , "index.html"));
+}) 
+
+app.listen(3000, ()=>{
+    console.log("App is started at port 3000");
+})
