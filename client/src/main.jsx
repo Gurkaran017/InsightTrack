66510f3a0b51862cd7d757fa1546361dabb9444b
Gurkaran017 <gurkaransingh017gh@gmail.com>
2025-05-20 21:33:40 +0530

    Updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-import './index.css';
+import './Index.css';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext'; // ✅ Import the provider
+import { AssetProvider } from "./contexts/AssetContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider> 
-      
-      <App />
+    <AuthProvider> {/* ✅ Wrap your App here */}
+      <AssetProvider>
+        <App />
+      </AssetProvider>
     </AuthProvider>
   </React.StrictMode>
 );
-//index.css
